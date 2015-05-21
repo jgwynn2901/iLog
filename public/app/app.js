32ccd029cfeb9843e39a2a407fe2944961edc87a
John Gwynn <jgwynn2901@yahoo.com>
2015-05-21 17:07:07 -0400

    Added UI-Router for navigation.

--- public/app/app.js
+++ public/app/app.js
@@ -2,7 +2,21 @@ define(['dataService', 'bugController'],
   function (dataService, bugController) {
     'use strict';
 
-    var app = angular.module('app', ['ui.bootstrap']);
+    var app = angular.module('app', ['ui.bootstrap', 'ui.router']);
+    app.config(function ($stateProvider, $urlRouterProvider) {
+
+      $urlRouterProvider.otherwise('/');
+      $stateProvider
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/',
+            templateUrl: 'views/list.html'
+        })
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit
+        })
+    });
 
     app.factory('dataService', dataService)
       .controller('bugController', bugController);
